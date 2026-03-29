6205f8144618f6454c2fafd03d6867da345cfc58
dean1013 <deandeans05@gmail.com>
2026-03-29 17:37:43 -0400

    deleting comments in mouse class

--- src/Mouse.java
+++ src/Mouse.java
@@ -5,13 +5,9 @@ import java.util.Iterator;
 
 public class Mouse extends Animal
 {
-    // The age at which a rabbit can start to breed.
     private static final int BREEDING_AGE = 2;
-    // The age to which a rabbit can live.
     private static final int MAX_AGE = 20;
-    // The likelihood of a rabbit breeding.
     private static final double BREEDING_PROBABILITY = 0.12;
-    // The maximum number of births.
     private static final int MAX_LITTER_SIZE = 4;
     
     
